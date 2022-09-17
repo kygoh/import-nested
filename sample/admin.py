0389783a825be94ac3667515efc52c590507c716
kygoh <gohkengyew@gmail.com>
2022-09-17 23:58:29 +0000

    add admin interface for models

--- sample/admin.py
+++ sample/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Asset, Site, SiteAsset
 
 # Register your models here.
+admin.site.register(Asset)
+admin.site.register(Site)
+admin.site.register(SiteAsset)
