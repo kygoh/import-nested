f8a13e0d35c7eca8961350e812d74571a753f0bb
kygoh <gohkengyew@gmail.com>
2022-09-18 00:12:55 +0000

    add import for site model

--- sample/admin.py
+++ sample/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportMixin
 from .models import Asset, Site, SiteAsset
+from .resources import SiteResource
 
 # Register your models here.
+class SiteAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = SiteResource
+
 admin.site.register(Asset)
-admin.site.register(Site)
+admin.site.register(Site, SiteAdmin)
 admin.site.register(SiteAsset)
