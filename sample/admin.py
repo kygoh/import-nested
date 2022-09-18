aac42bb4ec27b0dd6faf0e38a21e6d5c69080840
kygoh <gohkengyew@gmail.com>
2022-09-18 06:16:07 +0000

    add import for self reference site asset model

--- sample/admin.py
+++ sample/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportMixin
 from .models import Asset, Site, SiteAsset
-from .resources import SiteResource, AssetResource
+from .resources import SiteResource, AssetResource, SiteAssetResource
 
 # Register your models here.
 class SiteAdmin(ImportMixin, admin.ModelAdmin):
@@ -10,6 +10,9 @@ class SiteAdmin(ImportMixin, admin.ModelAdmin):
 class AssetAdmin(ImportMixin, admin.ModelAdmin):
     resource_class = AssetResource
 
+class SiteAssetAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = SiteAssetResource
+
 admin.site.register(Asset, AssetAdmin)
 admin.site.register(Site, SiteAdmin)
-admin.site.register(SiteAsset)
+admin.site.register(SiteAsset, SiteAssetAdmin)
--- sample/resources.py
+++ sample/resources.py
@@ -1,6 +1,7 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
-from .models import Site, Asset
+from .models import Site, Asset, SiteAsset
+from .widgets import CustomForeignKeyWidget
 
 class SiteResource(resources.ModelResource):
 
@@ -12,3 +13,11 @@ class AssetResource(resources.ModelResource):
 
     class Meta:
         model = Asset
+
+class SiteAssetResource(resources.ModelResource):
+    site = fields.Field(attribute='site', column_name='site', widget=ForeignKeyWidget(model=Site, field='site_no'))
+    asset = fields.Field(attribute='asset', column_name='asset', widget=ForeignKeyWidget(model=Asset, field='code'))
+    parent = fields.Field(attribute='parent', column_name='parent', widget=ForeignKeyWidget(model=SiteAsset, field='reference'))
+
+    class Meta:
+        model = SiteAsset
