cc637bb688563543b63b797e2a8fca663c1b36ff
kygoh <gohkengyew@gmail.com>
2022-09-18 05:14:47 +0000

    add import for self reference asset model

--- sample/admin.py
+++ sample/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from import_export.admin import ImportMixin
 from .models import Asset, Site, SiteAsset
-from .resources import SiteResource
+from .resources import SiteResource, AssetResource
 
 # Register your models here.
 class SiteAdmin(ImportMixin, admin.ModelAdmin):
     resource_class = SiteResource
 
-admin.site.register(Asset)
+class AssetAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = AssetResource
+
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(Site, SiteAdmin)
 admin.site.register(SiteAsset)
--- sample/resources.py
+++ sample/resources.py
@@ -1,7 +1,14 @@
 from import_export import resources, fields
-from .models import Site
+from import_export.widgets import ForeignKeyWidget
+from .models import Site, Asset
 
 class SiteResource(resources.ModelResource):
 
     class Meta:
         model = Site
+
+class AssetResource(resources.ModelResource):
+    parent = fields.Field(attribute='parent', column_name='parent', widget=ForeignKeyWidget(model=Asset, field='code'))
+
+    class Meta:
+        model = Asset
